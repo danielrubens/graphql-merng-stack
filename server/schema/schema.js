3520e8fdc7ed2c63372df344243fc31f3a380dbc
danielrubens <danielrubensdaniel@gmail.com>
2023-02-25 15:59:55 -0300

    chore: change query resolve syntax

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,5 @@
 const { project, clients } = require('../sampleData.js')
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql')
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql')
 
 const ClientType = new GraphQLObjectType({
     name: "Client",
@@ -14,12 +14,15 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve: (parent, args) => clients
+        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                return clients.find(client => client.id === args.id)
-            }
+            resolve: (parent, args) => clients.find(client => client.id === args.id)
+            
         }
     }
 })

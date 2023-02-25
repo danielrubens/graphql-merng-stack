887d312db82b5fc7af52d4a7ba59762838917339
danielrubens <danielrubensdaniel@gmail.com>
2023-02-25 16:29:44 -0300

    feat: connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const colors = require('colors')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
 const app = express()
+
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,

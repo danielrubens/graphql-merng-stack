3b4299188fb57794d3918b53dd781f1f7806d971
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 18:16:03 -0300

    feat: create spinner component

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,10 +1,12 @@
 import { useQuery } from '@apollo/client'
 import ClientRow from './ClientRow'
 import { GET_CLIENTS } from '../queries/clientQueries'
+import Spinner from './Spinner'
+
 
 const Clients = () => {
   const { loading, error, data } = useQuery(GET_CLIENTS)
-  if (loading) return <p>Loading...</p>
+  if (loading) return <Spinner />
   if (error) return <p>Something Went Wrong</p>
   return (
     <>

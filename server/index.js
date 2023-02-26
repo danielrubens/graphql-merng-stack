a3224e1f8357c2ccf10117954c18b1c578156d4e
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 17:51:46 -0300

    fix: cors on server index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
-const colors = require('colors')
+require('colors')
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
 const connectDB = require('./config/db')
@@ -10,9 +11,11 @@ const app = express()
 
 connectDB()
 
+app.use(cors())
+
 app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development'
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
 }))
 
 app.listen(port, console.log(`Server running on port ${port}`))

30e1ceb87782b181bbd80d46d27ac67a50e0eeb2
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 00:29:35 -0300

    feat: update project on mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -111,8 +111,33 @@ const mutation = new GraphQLObjectType({
         id: { type: GraphQLNonNull(GraphQLID) }
       },
       resolve: (parent, args) => Project.findByIdAndRemove(args.id)
+    },
+    updateProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: 'ProjectStatusUpdate',
+            values: {
+              new: { value: 'Not Started' },
+              progress: { value: 'In Progress' },
+              completed: { value: 'Completed' }
+            }
+          })
+        },
+        clientId: { type: GraphQLNonNull(GraphQLID) }
+      },
+      resolve: (parent, args) => Project.findByIdAndUpdate(args.id, {
+        $set: {
+          name: args.name, description: args.description, status: args.status
+        }
+      }, { new: true })
     }
   }
-})
+}
+)
 
 module.exports = new GraphQLSchema({ query: RootQuery, mutation })

4fe5a7b9eae0f8c8841c37a0bcec9834f846d484
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 17:50:10 -0300

    feat: gql and useQuery on clients component

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,6 +1,21 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_CLIENTS = gql`
+    query getClients{
+        clients {
+            id, name, email, phone
+        }
+    }`
+
 const Clients = () => {
+  const { loading, error, data } = useQuery(GET_CLIENTS)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Something Went Wrong</p>
   return (
-    <div>Clients</div>
+    <>
+        {!loading && !error && (<h1>Clients</h1>)}
+    </>
+
   )
 }
 

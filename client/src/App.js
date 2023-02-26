faf9cfb5b72533a8fc47906feecfd68bf6eb779a
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 18:48:01 -0300

    fix: merge incoming in memory cache to avoid data loss

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import { Header, Clients } from './components'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge: (existing, incoming) => incoming
+        },
+        projects: {
+          merge: (existing, incoming) => incoming
+        }
+      }
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
+  cache
 })
 
 function App() {

b514bf3a1263ade59401e304a5159c68e1a9fbc7
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 23:43:01 -0300

    feat: form inside add client button

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -4,6 +4,9 @@ import { useMutation } from '@apollo/client'
 
 
 const AddClientModal = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
   return (
     <>
         <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
@@ -21,7 +24,25 @@ const AddClientModal = () => {
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
                             </button>
                     </div>
-                    <div className="modal-body">...</div>
+                    <div className="modal-body">
+                        <form>
+                            <div className="mb-3">
+                                <label className="form-label">Name</label>
+                                <input type="text" className="form-control" id="name" value={name} 
+                                       onChange={({target}) => setName(target.value)}/>
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Email</label>
+                                <input type="email" className="form-control" id="email" value={email} 
+                                       onChange={({target}) => setEmail(target.value)}/>
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Phone</label>
+                                <input type="text" className="form-control" id="phone" value={phone} 
+                                       onChange={({target}) => setPhone(target.value)}/>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>

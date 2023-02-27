8cfa2a5a063df5b30cf77fe97ca312ab811a690f
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 23:27:49 -0300

    refactor: button data-bs-target add client modal

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -6,31 +6,24 @@ import { useMutation } from '@apollo/client'
 const AddClientModal = () => {
   return (
     <>
-        <button type="button" className="btn btn-secondary" data-toggle="modal" data-target="#addClientModal">
+        <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
             <div className="d-flex align-items-center">
                 <FaUser className="icon"/>
                 <div>Add Client</div>
             </div>
         </button>
 
-        <div className="modal fade" id="addClientModal" role="dialog" aria-labelledby="addClientModalLabel" aria-hidden="true">
-        <div className="modal-dialog" role="document">
-            <div className="modal-content">
-            <div className="modal-header">
-                <h5 className="modal-title" id="addClientModalLabel">Modal title</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
+        <div className="modal fade" id="addClientModal" aria-labelledby="addClientModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                            <h5 className="modal-title" id="addClientModalLabel">Add Client</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                            </button>
+                    </div>
+                    <div className="modal-body">...</div>
+                </div>
             </div>
-            <div className="modal-body">
-                ...
-            </div>
-            <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
-            </div>
-        </div>
         </div>
     </>
   )

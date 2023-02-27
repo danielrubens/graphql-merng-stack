e2f621e21e856c5ac601cc3418dc4aa5a8d0f6d8
danielrubens <danielrubensdaniel@gmail.com>
2023-02-27 00:36:27 -0300

    feat: onSubmit add client

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,23 +1,41 @@
 import {  useReducer } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { useMutation } from '@apollo/client'
+import { ADD_CLIENT } from '../mutations/clientMutations'
+import { GET_CLIENTS } from '../queries/clientQueries'
 
 
 const AddClientModal = () => {
   const initialState = {name: "", email: "", phone: "" }
   const [state, setState] = useReducer((state, action) => ({...state, ...action }), initialState)
   const keys = Object.keys(state)
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: {name: state.name, email: state.email, phone: state.phone},
+    update: (cache, { data: { addClient } }) => {
+        const { clients } = cache.readQuery({ query: GET_CLIENTS })
+        cache.writeQuery({
+            query: GET_CLIENTS,
+            // data: { clients: clients.concat([addClient]) }
+            data: { clients: [...clients, addClient] }
+        })
+    }
+  })
   
   const onSubmit = (e) => {
+    if(keys.some((key) => state[key] === '')) {
+        e.preventDefault()
+        return alert('Please, fill in all fields')
+    }
+    addClient(state.name, state.email, state.phone)
     e.preventDefault()
-    console.log(state)
   }
 
   return (
     <>
         <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
             <div className="d-flex align-items-center">
-                <FaUser className="icon"/>
+                <FaUser className="icon" />
                 <div>Add Client</div>
             </div>
         </button>

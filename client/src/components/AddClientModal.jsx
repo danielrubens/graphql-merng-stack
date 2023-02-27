6aefca498f9a91fba38fdc52cd5f7a16dea558a7
danielrubens <danielrubensdaniel@gmail.com>
2023-02-26 23:49:39 -0300

    refactor: replace useState for useReducer

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import {  useReducer } from 'react'
 import { FaUser } from 'react-icons/fa'
 import { useMutation } from '@apollo/client'
 
 
 const AddClientModal = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
+  const initialState = {name: "", email: "", phone: "" }
+  const [state, setState] = useReducer((state, action) => ({...state, ...action }), initialState)
+  const keys = Object.keys(state)
+
   return (
     <>
         <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addClientModal">
@@ -26,21 +27,13 @@ const AddClientModal = () => {
                     </div>
                     <div className="modal-body">
                         <form>
-                            <div className="mb-3">
-                                <label className="form-label">Name</label>
-                                <input type="text" className="form-control" id="name" value={name} 
-                                       onChange={({target}) => setName(target.value)}/>
-                            </div>
-                            <div className="mb-3">
-                                <label className="form-label">Email</label>
-                                <input type="email" className="form-control" id="email" value={email} 
-                                       onChange={({target}) => setEmail(target.value)}/>
-                            </div>
-                            <div className="mb-3">
-                                <label className="form-label">Phone</label>
-                                <input type="text" className="form-control" id="phone" value={phone} 
-                                       onChange={({target}) => setPhone(target.value)}/>
-                            </div>
+                            {keys.map((_, index) =>(
+                                <div className="mb-3">
+                                    <label className="form-label">{keys[index]}</label>
+                                    <input type="text" className="form-control" id={keys[index]} 
+                                           onChange={({target}) => setState({[keys[index]]: target.value})}/>
+                                </div>
+                            ))}
                         </form>
                     </div>
                 </div>

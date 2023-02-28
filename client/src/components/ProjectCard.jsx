7852088d484aad826c96e1a4ddfbf626401d4032
danielrubens <danielrubensdaniel@gmail.com>
2023-02-27 21:57:46 -0300

    feat: strong status on project card

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -7,6 +7,7 @@ const ProjectCard = ({project}) => {
                     <h5 className="card-title">{project.name}</h5>
                     <a className="btn btn-light" href={`/projects/${project.id}`}>View</a>
                 </div>
+                <p className="small">Status: <strong>{project.status}</strong></p>
             </div>
         </div>
     </div>
